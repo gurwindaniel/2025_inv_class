146ee8880f7562f5f16aa1ccc37bc869bf2d1a45
gurwindaniel <gurwindaniel@gmail.com>
2025-02-05 11:01:54 +0530

    single view

--- play.js
+++ play.js
@@ -9,10 +9,36 @@
 
 // printing(name);
 
-console.log('first');
+// console.log('first');
 
-setTimeout(() => {
-  console.log('second');
-}, 1000);
+// setTimeout(() => {
+//   console.log('second');
+// }, 1000);
 
-console.log('third');
+// console.log('third');
+
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+
+// app.use((req, res, next) => {
+//   console.log('middleware Function');
+//   next();
+// });
+
+// app.get('/', (req, res) => {
+//   console.log('Home');
+//   res.send('Home');
+// });
+
+// const middleware = (req, res, next) => {
+//   console.log('middleware Function as Function');
+//   next();
+// };
+
+// app.get('/about', middleware, (req, res) => {
+//   console.log('About');
+//   res.send('About');
+// });
+
+// app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
